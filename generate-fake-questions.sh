c65c9c41ab36513710244bd9d917a7331426125f
Maciej Korzen <maciek@korzen.org>
2018-02-13 22:37:45 +0100

    Bugfix in generate-fake-questions.sh and new fake questions.

--- generate-fake-questions.sh
+++ generate-fake-questions.sh
@@ -8,9 +8,9 @@ for i in {1..200}; do
 			mkdir -p "$i" || exit 1
 		fi
 	done
-	cat $(find /usr/share/doc -type f -name README | shuf | head -n 1) /dev/null | tr -d '_*\n=<>-|/' | sed -r -e 's,[ \t]+, ,g' | cut -b 1-$[RANDOM % 200 + 20] > "${testsubdir}/question"
+	cat $(find /usr/share/doc -type f -name README | shuf | head -n 1) /dev/null | tr -d '_*\n=<>\|/-' | sed -r -e 's,[ \t]+, ,g' | cut -b 1-$[RANDOM % 200 + 20] > "${testsubdir}/question"
 	for j in {1..4}; do
-		cat $(find /usr/share/doc -type f -name README | shuf | head -n 1) /dev/null | sed -r -e "s,(.{$[RANDOM % 10 + 5]}),\1 ,g" | tr -d '_*\n=<>-|/' | sed -r -e 's,[ \t]+, ,g' | cut -b 1-$[RANDOM % 100 + 10] > "${testsubdir}/answers/0/${j}"
+		cat $(find /usr/share/doc -type f -name README | shuf | head -n 1) /dev/null | sed -r -e "s,(.{$[RANDOM % 10 + 5]}),\1 ,g" | tr -d '_*\n=<>\|/-' | sed -r -e 's,[ \t]+, ,g' | cut -b 1-$[RANDOM % 100 + 10] > "${testsubdir}/answers/0/${j}"
 	done
-	cat $(find /usr/share/doc -type f -name README | shuf | head -n 1) /dev/null | sed -r -e "s,(.{$[RANDOM % 10 + 5]}),\1 ,g" | tr -d '_*\n=<>-|/' | sed -r -e 's,[ \t]+, ,g' | cut -b 1-$[RANDOM % 100 + 15] > "${testsubdir}/answers/1/0"
+	cat $(find /usr/share/doc -type f -name README | shuf | head -n 1) /dev/null | sed -r -e "s,(.{$[RANDOM % 10 + 5]}),\1 ,g" | tr -d '_*\n=<>\|/-' | sed -r -e 's,[ \t]+, ,g' | cut -b 1-$[RANDOM % 100 + 15] > "${testsubdir}/answers/1/0"
 done
